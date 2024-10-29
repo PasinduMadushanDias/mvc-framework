99c3b1ac48a40f9bfd621cf5507f77532afdf845
PasinduMadushan10 <pmdiasw@gmail.com>
2024-10-30 03:36:28 +0530

    add new file

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
-<?php
+<?php 
 
-$heading = 'Home';
+require 'function.php';
 
- require "views/index.view.php";
+$uri =parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+];
+
+function abort() {
+    
+    http_response_code(404);
+
+    require 'views/404.php';
+
+    die();
+}
+
+if (array_key_exists($uri,$routes)) {
+    require $routes[$uri];
+}else{
+    abort();
+}
